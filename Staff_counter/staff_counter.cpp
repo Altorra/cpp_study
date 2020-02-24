45827e648f875038db414ac19c28c8c6c84400c5
Lukiyan Terekhov <cassiel777@yahoo.com>
2020-02-24 19:47:31 +0000

    first commit

---
+++ Staff_counter/staff_counter.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+using namespace std;
+
+int main() {
+    int a = 10;
+    int b = -20;
+    double c = 10.2;
+    char d = 'a';
+    string e = "hello world";
+
+    vector <int> f = {1, 2, 3};
+    
+    map<char, int> g;
+    
+    map<char, int> r;
+    r['b'] = 1;
+    g['a'] = 1;
+};
