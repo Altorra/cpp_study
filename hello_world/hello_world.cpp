5efc9987590ba7ac54ca4d92618574067fc6ce4d
Lukiyan Terekhov <cassiel777@yahoo.com>
2020-03-02 21:45:44 +0000

    first commit

--- hello_world/hello_world.cpp
+++ hello_world/hello_world.cpp
@@ -9,19 +9,84 @@ struct person {
     string surname;
     int age;
 };
+//struct warcraft {
+//    string charcters;
+//    string levels;
+//    int 
+//
+//
+//};
+
+
 
 int main() {
-    /*int x = -5;
+    int x = -5;
     double i = 3.14;
     bool er = false;
     char symbol = 'a';
     string hw = "hello world";
     vector<int> nums = {1, 3, 4};
     cout << nums.size();*/
-    map<string, int> name_to_value;
-    name_to_value["one"] = 1;
-    name_to_value["two"] = 2;
-    cout << "two is" << name_to_value["two", "one"];
+    //map<string, int> name_to_value;
+    //name_to_value["one"] = 1;
+    //name_to_value["two"] = 2;
+    cout << "two is" << name_to_value["two", "one"];*/
+
+
     return 0;
+    // string age = "23";
+    // age = "23"
+    //int aa = 23;
+    // aa = 23
+    // aa = 0.12
+    //float aa = 0.12;
+    //char aa = 'a';
+    //aa = 'a'
+ // C++
+    //# python                     
+    // int a[] = {1, 2, 3};       
+    // a = [1, 2, 3]
+    // print(a[0])
+    // a = (1, 2, 3) tuple dosent change its meaning
+    //vector<string> age_of_game;
+    //age_of_game.push_back("Hello world");
+
+
+
+    // SET
+    // a = [6, 1, 2, 3, 3, 5, 6]
+    // b = set(a)
+    // b = set([6, 1, 2, 3, 3, 5, 6]) 6,1,2,3,5
+    // b = set()
+    // b.add(1) - add to set
+
+    // DICT
+    //       key   value
+    // a = {'age': 10, 'name': 'Ivan'}
+    //b = {}
+    //b= {'budget': 100000, 'movie': 'Alien'}
+    // b['actor'] = 'xxx'
+    // if 'actor' in b:
+    //     print('exist')
+    // else:
+    //     print('not exist')
+    // del b['actor']
+    //
+    // for k, v in b.items():
+    //     print('key', k)
+    //     print('value', v)
+    //
+    //    key      value
+    //
+    //  'budget': 100000   b.items()
+    //  'movie': 'Alien'   b.items()
+    //  'actor': 'xxx'     b.items()
+    //
+    //
+    //
+}
+
+
+
 }
 
